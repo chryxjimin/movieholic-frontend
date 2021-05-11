5d200150acd511ebb65226c03cc5043ed5f3088f
Your Username <you@example.com>
2021-05-11 18:01:31 -0400

    edited app.js to connect to server

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class App extends React.component {
+class App extends Component {
+
+  componentDidMount() {
+    fetch('http://localhost:3000/api/v1/reviews')
+    .then(res => res.json())
+    .then(data => console.log(data[0].description))
+  }
+
   render() {
-    
     return (
       <div className="App">
         App
