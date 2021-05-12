386a04de9fd3e4756d04dacef7781d43fb40695b
Your Username <you@example.com>
2021-05-12 12:17:58 -0400

    added compose and created store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'; 
 import App from './App';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+// compose - Composes functions from right to left.
+// This is a functional programming utility, and is included in Redux as a convenience. 
+// You might want to use it to apply several store enhancers in a row.
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+//store is where you're storing data and you have to pass it as a prop to the Provider
+//reducers tell us what to do with our store based on certain actions
+//and will return a new version of that store
+
 ReactDOM.render(
-    <React.StrictMode>
+    <Provider store={store}>
         <App /> 
-    </React.StrictMode>,
+    </Provider >,
 document.getElementById('root')
 );
