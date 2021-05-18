5e671ef25d31fcb34103daf19076bab14b81a8a8
Your Username <you@example.com>
2021-05-17 22:17:19 -0400

    refactored deleteReview to work

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,19 +1,26 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { deleteReview } from '../redux/actions/deleteReview'
 
 const Reviews = (props) => {
-
-    // handleDelete = (event) => {
-    //     //need an action creator
-    // }
+    // console.log(props)
+    const handleDelete = (review) => {
+        // debugger;
+        props.deleteReview(review.id, review.movie_id); 
+        // this.setState({
+        //     description: ''
+        // })
+    }
 
     return (
         <div>
+            {console.log(props.reviews)}
             { props.reviews && props.reviews.map(review => 
-                <li key={review.id}>{review.description}<button onClick={this.handleDelete}>Delete</button></li>
+                <li key={review.id}>{review.description}<button onClick={() => handleDelete(review)}>Delete</button></li>
                 )}
         </div>
     )
 }
 
 
-export default Reviews;
+export default connect(null, { deleteReview })(Reviews);
