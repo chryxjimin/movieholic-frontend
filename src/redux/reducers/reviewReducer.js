eac53d4e4b9acc99a475f3698ef22a397e6140fc
Your Username <you@example.com>
2021-05-25 11:46:02 -0400

    cleaned up reviewReducer

--- src/redux/reducers/reviewReducer.js
+++ src/redux/reducers/reviewReducer.js
@@ -1,12 +1,8 @@
 function reviewReducer(state = {reviews: []}, action) {
-
     switch (action.type) {
         case 'ADD_REVIEW': 
             return {...state, reviews: [...state.reviews, action.payload]}
         default:
             return state
     }
-    // return action.payload
-    //to see if that our action was coming in correctly and that the store
-    // could properly be updated by our reducer
 }
