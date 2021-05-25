099bff7a89bf8ff885f4796d8ca8ef476fbd8bc6
Your Username <you@example.com>
2021-05-25 11:23:20 -0400

    cleaned up Movie.js

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReviewsContainer from '../containers/ReviewsContainer'
-// import{ Route, Link } from 'react-router-dom'
+
 
 const Movie = (props) => {
 
 
     let movie = props.movies[props.match.params.id - 1]
-    console.log(`movie`, movie)
+ 
     return (
         <div>
             <p>{movie.title} <br></br> {movie.year}</p>
@@ -32,34 +32,3 @@ const Movie = (props) => {
 
 
 export default Movie;
-
-
-// if (movie? movie : null) {
-//     return (
-
-//     <div>
-//         <p>{movie.title} <br></br> {movie.year}</p>
-//         <p>{movie.rated}</p>
-//         <img src={movie.poster} height="300" width="200" />
-//         <p>Released: {movie.released}</p>
-//         <p>Runtime: {movie.runtime}</p>
-//         <p>Genre: {movie.genre}</p>
-//         <p>Director: {movie.director}</p>
-//         <p>Writer: {movie.writer}</p>
-//         <p>Actors: {movie.actors}</p>
-//         <p>Language: {movie.language}</p>
-//         <p>Country: {movie.country}</p>
-//         <p>Awards: {movie.awards}</p>
-//         <p>imdbRating: {movie.imdbRating}</p>
-//         <p>imdbVotes: {movie.imdbVotes}</p>
-//         <p>Box Office: {movie.box_office}</p>
-//         <p>Production: {movie.production}</p>
-//         <ReviewsContainer movie={movie} />
-//     </div>
-//     )
-// }
-
-
-
-
-//where we show the specifc movie's information
