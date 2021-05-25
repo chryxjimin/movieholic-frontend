848e2cc41b60276aaa6ed2a35a7a17097876215e
Your Username <you@example.com>
2021-05-25 11:20:13 -0400

    cleand up MoviesContainer

--- src/containers/MoviesContainer.js
+++ src/containers/MoviesContainer.js
@@ -6,9 +6,7 @@ import MovieApi from '../components/MovieApi'
 import ReviewForm from '../components/ReviewForm'
 import { fetchMovies } from '../redux/actions/fetchMovies'
 import { Route, Switch  } from 'react-router-dom'
-// import { Redirect } from 'react-router-dom'
 
-//switch returns the first thing that matches
 
 class MoviesContainer extends Component {
 
@@ -21,8 +19,6 @@ class MoviesContainer extends Component {
         console.log(movie)
         if (movie !== undefined ) {
             return <Movie {...routerProps} movies={this.props.movies} />
-            // } else {
-            // return <Redirect to='/movies' />
         }
     }
 
@@ -51,6 +47,3 @@ const mapStateToProps = state => {
 }
 
 export default connect(mapStateToProps, { fetchMovies })(MoviesContainer);
-//As the first argument passed in to connect , mapStateToProps is used for selecting
-// the part of the data from the store that the connected component needs.
-// It is called every time the store state changes.
