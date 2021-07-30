4ce3e740672edff996580e917f2246e6b1823897
Your Username <you@example.com>
2021-07-30 19:30:32 -0400

    cleaned up code and delete button is now working

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,12 +1,10 @@
-// import Button from 'react-bootstrap/Button'
-// import { Button, Alert, Card } from 'react-bootstrap'
 import { Card } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import React from 'react'
 import Review from '../components/Review'
 import { connect } from 'react-redux'
 import { deleteReview } from '../redux/actions/deleteReview'
-// import { Link } from 'react-router-dom'
+
 
 
 
@@ -35,32 +33,5 @@ class Reviews extends React.Component {
 
 }
 
-// class Review extends React.Component {
-//     state = {
-//         count: 0
-//     }
 
-//      handleUpvote = (review) => {
-        
-//         this.setState({
-//             count: this.state.count + 1
-//         })
-//     }
-    
-//      handleDelete = (review) => {
-//         this.props.deleteReview(review.id, review.movie_id); 
-//     }
-//     render(){
-//        const movieId = this.props.review.movie_id
-//         return <>
-
-//                 <li key={this.props.review.id}>{this.props.review.description}<button onClick={() => this.handleDelete(this.props.review)}>Delete</button><button onClick={() => this.handleUpvote(this.props.review)}>{this.state.count}</button></li>
-//                 <Link to={`/movies/${movieId}/reviews/new`}>Write a Review</Link>
-//                  <Button>Test Button</Button>
-//                </>
-//     }
-// }
-
-
-// export default connect(null, { deleteReview })(Reviews);
 export default Reviews;
