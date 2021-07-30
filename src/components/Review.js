7249e96fcf855fb521b44484f73ac0c9396833ac
Your Username <you@example.com>
2021-07-30 19:25:58 -0400

    separated Review class from Reviews

---
+++ src/components/Review.js
@@ -0,0 +1,35 @@
+
+import React from 'react'
+import { connect } from 'react-redux'
+import { deleteReview } from '../redux/actions/deleteReview'
+import { Link } from 'react-router-dom'
+import { Button } from 'react-bootstrap'
+
+class Review extends React.Component {
+    state = {
+        count: 0
+    }
+
+     handleUpvote = (review) => {
+        
+        this.setState({
+            count: this.state.count + 1
+        })
+    }
+    
+     handleDelete = (review) => {
+        this.props.deleteReview(review.id, review.movie_id); 
+    }
+    render(){
+       const movieId = this.props.review.movie_id
+        return <>
+
+                <li key={this.props.review.id}>{this.props.review.description}<button onClick={() => this.handleDelete(this.props.review)}>Delete</button><button onClick={() => this.handleUpvote(this.props.review)}>{this.state.count}</button></li>
+                <Link to={`/movies/${movieId}/reviews/new`}>Write a Review</Link>
+                 <Button>Test Button</Button>
+               </>
+    }
+}
+
+
+export default connect(null, { deleteReview })(Review);
--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,10 +1,12 @@
 // import Button from 'react-bootstrap/Button'
-import { Button, Alert, Card } from 'react-bootstrap'
+// import { Button, Alert, Card } from 'react-bootstrap'
+import { Card } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import React from 'react'
+import Review from '../components/Review'
 import { connect } from 'react-redux'
 import { deleteReview } from '../redux/actions/deleteReview'
-import{ Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 
 
@@ -19,11 +21,13 @@ class Reviews extends React.Component {
         const movieId = this.props.reviews.map(movie => movie.movie_id)[0]
         return (
             <div>
+                    
                         { this.props.reviews && this.props.reviews.map(review => {
                             return <Card>
                                         <Card.Body><Review review={review}/></Card.Body>
                                    </Card>
                         })}
+                
                
             </div>
         )
@@ -31,31 +35,32 @@ class Reviews extends React.Component {
 
 }
 
-class Review extends React.Component {
-    state = {
-        count: 0
-    }
+// class Review extends React.Component {
+//     state = {
+//         count: 0
+//     }
 
-     handleUpvote = (review) => {
+//      handleUpvote = (review) => {
         
-        this.setState({
-            count: this.state.count + 1
-        })
-    }
+//         this.setState({
+//             count: this.state.count + 1
+//         })
+//     }
     
-     handleDelete = (review) => {
-        this.props.deleteReview(review.id, review.movie_id); 
-    }
-    render(){
-       const movieId = this.props.review.movie_id
-        return <>
+//      handleDelete = (review) => {
+//         this.props.deleteReview(review.id, review.movie_id); 
+//     }
+//     render(){
+//        const movieId = this.props.review.movie_id
+//         return <>
 
-                <li key={this.props.review.id}>{this.props.review.description}<button onClick={() => this.handleDelete(this.props.review)}>Delete</button><button onClick={() => this.handleUpvote(this.props.review)}>{this.state.count}</button></li>
-                <Link to={`/movies/${movieId}/reviews/new`}>Write a Review</Link>
-                 <Button>Test Button</Button>
-               </>
-    }
-}
+//                 <li key={this.props.review.id}>{this.props.review.description}<button onClick={() => this.handleDelete(this.props.review)}>Delete</button><button onClick={() => this.handleUpvote(this.props.review)}>{this.state.count}</button></li>
+//                 <Link to={`/movies/${movieId}/reviews/new`}>Write a Review</Link>
+//                  <Button>Test Button</Button>
+//                </>
+//     }
+// }
 
 
-export default connect(null, { deleteReview })(Reviews);
+// export default connect(null, { deleteReview })(Reviews);
+export default Reviews;
