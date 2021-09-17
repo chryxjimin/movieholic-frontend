f0986d73c0d6a093932335244d4d10f1de40f5e9
Your Username <cleex1999@gmail.com>
2021-09-17 15:11:19 -0400

    edited button

--- src/components/Review.js
+++ src/components/Review.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { deleteReview } from '../redux/actions/deleteReview'
 import { Link } from 'react-router-dom'
+// import ButtonGroup from 'react-bootstrap/ButtonGroup'
 
 
 class Review extends React.Component {
@@ -24,10 +25,12 @@ class Review extends React.Component {
        const movieId = this.props.review.movie_id
         return <>
                 <Card>
-                        <li key={this.props.review.id}>{this.props.review.description} <Button variant="secondary" onClick={() => this.handleDelete(this.props.review)}>Delete</Button> <Button variant="secondary" onClick={() => this.handleUpvote(this.props.review)}>{this.state.count}</Button></li>
-                        <Link to={`/movies/${movieId}/reviews/new`}>Write a Review</Link>
+                    {/* <ButtonGroup size="sm"> */}
+                        <li key={this.props.review.id}>{this.props.review.description} <Button variant="outline-secondary" onClick={() => this.handleDelete(this.props.review)}>Delete</Button> <Button variant="outline-secondary" onClick={() => this.handleUpvote(this.props.review)}>{this.state.count}</Button></li>
+                       
+                    {/* </ButtonGroup> */}
                 </Card>
-                
+                <Link to={`/movies/${movieId}/reviews/new`}>Write a Review</Link>
                </>
     }
 }
